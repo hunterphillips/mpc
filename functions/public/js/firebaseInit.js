6a579c968a6f8749de1ebadf85d97b644fe8c92c
Hunter Phillips <hkphillips42@gmail.com>
2021-02-07 12:37:42 -0600

    Update firebaseInit.js

--- functions/public/js/firebaseInit.js
+++ functions/public/js/firebaseInit.js
@@ -1,15 +1,15 @@
 // // Your web app's Firebase configuration
 // var firebaseConfig = {
-// 	apiKey: "AIzaSyDCHI3BCqZgDPRozmC02TH1BMmx4QioHH8",
+// 	apiKey: "1234",
 // 	authDomain: "sampler-799e4.firebaseapp.com",
 // 	databaseURL: "https://sampler-799e4.firebaseio.com",
 // 	projectId: "sampler-799e4",
 // 	storageBucket: "sampler-799e4.appspot.com",
-// 	messagingSenderId: "407258037733",
-// 	appId: "1:407258037733:web:d30b37cdf148e992c5030c",
-// 	measurementId: "G-RBPKKRPC9Z",
+// 	messagingSenderId: "1234",
+// 	appId: "1234",
+// 	measurementId: "1234",
 // };
 // // Initialize Firebase
 // firebase.initializeApp(firebaseConfig);
 // firebase.analytics();
-// Stripe.setPublishableKey("pk_test_edOrElul43Z9ukkHCDfy68gG00UgG9OFJP");
+// Stripe.setPublishableKey("1234");
